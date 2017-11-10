392e4728dd5c3a8762f9a64ad386e3fdcf2a3906
gTay <teambuckfast@gmail.com>
2017-11-10 14:17:29 +0000

    Binary tip amended
    The allignment and test for the binary tip has been amended and realligned

--- js/main.js
+++ js/main.js
@@ -15,30 +15,25 @@ Array.prototype.shuffle = function() {
 
 // List of Java tips
 var tipsList = [
-    "<span class='code'>GIGO: <br></span>If you put <span class='code'>G</span> arbage <span class='code'>I</span>n you will get <span class='code'>G</span>arbage <span class='code'>O</span>ut!",
+		"<span class='code'>GIGO:</span><br>If you put <span class='code'>G</span> arbage <span class='code'>I</span>n you will get <span class='code'>G</span>arbage <span class='code'>O</span>ut!",
 
-    "The binary system is known as a <span class='code-block'>base 2</span> system<br><span class='code'>1  0  1  0  1  0 0 0</span><span ='code.5'>= binary</span><br> <span class='code-block'>x  x  x  x  x x x  x<br>128 64 32 16 8 4 2 1</span>, <span class='code-block'>0</span>",
+		"The binary system is as a base 2 system<span class='code-block'><br>  &nbsp1 &nbsp0 &nbsp1 &nbsp0  &nbsp1  0 0 0 = binary<br> 128 64 32 16 8 4 2 1 = Denary/Decimal</span>",
 
-    "Declare a function with<br/> <span class='code-block'>function <em>myFunctionName</em>() { <br>&nbsp;&nbsp;...<br> }</span>",
+		"<span class='code'>if</span>/<span class='code'>else</span> statements look like <br/> <span class='code-block'>if (<em>condition</em>) { <br/>&nbsp;&nbsp;...<br/>} else { <br/>&nbsp;&nbsp;...<br/>}",
 
-    "<span class='code'>if</span>/<span class='code'>else</span> statements look like <br/> <span class='code-block'>if (<em>condition</em>) { <br/>&nbsp;&nbsp;...<br/>} else { <br/>&nbsp;&nbsp;...<br/>}",
+		"The <span class='code'>&&</span> operator means both things must be true: <br/> <span class='code-block'> true && true = true<br>true && false = false<br>false && false = false</span>",
 
-    "You can return the result of a function into a variable with <span class='code'>return</span>:<br><span class='code-block'>function timesFive(inputNumber) {<br> &nbsp;&nbsp;return inputNumber * 5; <br>}<br>console.log(timesFive(6));<br>// Output: 30</span> ",
+		"The <span class='code'>||</span> operator means either can be true: <br/> <span class='code-block'> true || true = true<br>true || false = true<br>false || false = false</span>",
 
-    "The <span class='code'>&&</span> operator means both things must be true: <br/> <span class='code-block'> true && true = true<br>true && false = false<br>false && false = false</span>",
+		"A <span class='code'>for</span> has three condtions: a start condition, a stop condition, and an iterator: <br> <span class='code-block'>for (var i = 0; i < myArray.length; i++) { <br>&nbsp;&nbsp;...<br>}</span>",
 
-    "The <span class='code'>||</span> operator means either can be true: <br/> <span class='code-block'> true || true = true<br>true || false = true<br>false || false = false</span>",
+		"To interpolate a variable into a string, use the <span class='code'>+</span> operator, like this: <br> <span class='code-block'>String myName = 'Java'; 'Hello, my name is ' + myName;</span>",
 
-    "A <span class='code'>for</span> has three condtions: a start condition, a stop condition, and an iterator: <br> <span class='code-block'>for (var i = 0; i < myArray.length; i++) { <br>&nbsp;&nbsp;...<br>}</span>",
+		"To generate a random number, use Java's built in function <span class='code'>Math.random()</span>.",
 
-    "To interpolate a variable into a string, use the <span class='code'>+</span> operator, like this: <br> <span class='code-block'>String myName = 'Java'; 'Hello, my name is ' + myName;</span>",
+		"Adding two numbers in Java <span class='code-block'><br> <span class ='code-red'>public static int </span>addNo (<span class ='code-red'>final int</span> num1, <span class ='code-red'>final int</span> num2){<br> <span class ='code-red'>int</span> answer = num1 + num2;<br> <span class ='code-red'>return</span> answer;<br>}</span>",
 
-    "To generate a random number, use Java's built in function <span class='code'>Math.random()</span>.",
-
-    "Adding two numbers in Java <span class='code-block'><br> <span class ='code-red'>public static int </span>addNo (<span class ='code-red'>final int</span> num1, <span class ='code-red'>final int</span> num2){<br> <span class ='code-red'>int</span> answer = num1 + num2;<br> <span class ='code-red'>return</span> answer;<br>}</span>",
-
-    "Java has 8 primitive types<br><span class='code-block'> byte, short, int, lont, float, double, char and boolean</span> "
-
+		"Java has 8 primitive types<br><span class='code-block'> byte, short, int, long, float, double, char and boolean</span> "
 
 ];
 
